e4a651aeff686e964560eab5229f4cb9b9ee0666
Vishwas <baghel.vishwas0@gmail.com>
2022-06-29 17:54:35 +0530

    [Vishwas]add: Mood Analyzer Program Added

---
+++ MoodAnalyzerProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MoodAnalyzerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Mood Analyzer Program \n=====================================");
+        }
+    }
+}
